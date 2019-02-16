1853c7bb4b15f135939db91bd45b8142cb648239
Likhit <47667163+airwars2@users.noreply.github.com>
2019-02-16 15:27:24 +0530

    Add files via upload

---
+++ Service/app/src/main/java/nareshit/com/service/MainActivity.java
@@ -0,0 +1,30 @@
+package nareshit.com.service;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    public void stopService(View view) {
+        Intent i = new Intent(this, MyService.class);
+        stopService(i);
+    }
+
+    public void moveToNext(View view) {
+        Intent i = new Intent(this, SecondActivity.class);
+        startActivity(i);
+    }
+}
